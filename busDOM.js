6aa67e614f315e2003f0add65dd162a39c94432d
Saffah Khatib <skhatib1910@gmail.com>
2023-05-31 10:17:14 +0200

    planning and setup

---
+++ busDOM.js
@@ -0,0 +1,3 @@
+//in the dom add references, event listner and instance
+//function to statemnt if peak or off peak is selcted it should do what the function says
+//function if the return trip is selected it should 
---
+++ busFactory.js
@@ -0,0 +1,11 @@
+// the factory  function should be able to set points and location and calculate the following :
+//1. number of trips
+//2.single price per trip
+//3. number of return trips
+//4. price per trip
+//need variable for total points
+//there should be function that will calculate how much each loaction will cost using conditional statements
+//Khayelitsha = R40 , Dunoon = R25 , Mitchells Plain = R30
+//another function should calculate how much increase to add if it is peak time (if the user selects a location in the previous function it should add certain percnetage to that amount)
+//to calculate the amount increased divide the normal price by 100 multiply that by 25% and then add that to the original amount
+//to each of these functions need to another functon that will only return the counters
