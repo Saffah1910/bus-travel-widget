84e48462831986ed353b331a53dd4883b6cf160a
Saffah Khatib <skhatib1910@gmail.com>
2023-05-31 13:53:49 +0200

    intial commit

--- busFactory.js
+++ busFactory.js
@@ -9,3 +9,79 @@
 //another function should calculate how much increase to add if it is peak time (if the user selects a location in the previous function it should add certain percnetage to that amount)
 //to calculate the amount increased divide the normal price by 100 multiply that by 25% and then add that to the original amount
 //to each of these functions need to another functon that will only return the counters
+
+
+function BusTicket() {
+    var numSingleTrip = 0;
+    var priceSingle = 0;
+    var numReturnTrip = 0;
+    var pricesReturn = 0;
+    // var peak = 0;
+
+    function singleTrip(points, location) {
+        if (points >= 40 && location == "khayelitsha") {
+            numSingleTrip += 1;
+            priceSingle += 40;
+            return true
+        }
+        if (points >= 25 && location == "dunoon") {
+            numSingleTrip += 1;
+            priceSingle += 25;
+            return true
+
+        }
+        if (points >= 30 && location == "mitchellsPlain") {
+            numSingleTrip += 1;
+            priceSingle += 30;
+            return true
+
+        }
+        else {
+            return "not enough points to travel to " + location
+        }
+
+    }
+    function amountOfSingle() {
+        return numSingleTrip
+    }
+    function totalPriceOfSingle() {
+        return priceSingle
+    }
+    function returnTrip() {
+        //if the number of single trips is even it should divide the single trips by 2 else if it is odd????
+        if (numSingleTrip % 2 === 0) {
+
+        }
+        numReturnTrip = numSingleTrip / 2
+        pricesReturn = priceSingle *= 2;
+        // else if(numSingleTrip % 2 ==!0){
+        //     numReturnTrip = numSingleTrip /2 
+        // }
+
+    }
+    function getReturnPrice() {
+        return pricesReturn
+    }
+    function amountOfReturn() {
+        return numReturnTrip
+    }
+    function addPeak() {
+
+        numSingleTrip /= 100;
+        numSingleTrip *= 25;
+       numSingleTrip += numSingleTrip;
+        return numSingleTrip *= 100
+
+
+    }
+    return {
+        singleTrip,
+        amountOfSingle,
+        totalPriceOfSingle,
+        returnTrip,
+        getReturnPrice,
+        amountOfReturn,
+        addPeak
+    }
+
+}
---
+++ tests/bus.test.js
@@ -0,0 +1,100 @@
+
+describe("The Bus Ticket function", function () {
+
+    describe("use points and select location", function () {
+        it("if the user enters 40 points or more and selects khayelitsha it should return true", function () {
+            let busTravel = BusTicket();
+            assert.equal(true, busTravel.singleTrip(40, "khayelitsha"));
+        });
+        it("if the user enters 30 points or more and selects mitchellsPlain it should return true", function () {
+            let busTravel = BusTicket();
+            assert.equal(true, busTravel.singleTrip(30, "mitchellsPlain"));
+        });
+        it("if the user enters 23 points or less and selects dunoon it should return false since the points are not enough", function () {
+            let busTravel = BusTicket();
+            assert.equal("not enough points to travel to dunoon", busTravel.singleTrip(23, "dunoon"));
+        });
+
+    });
+    describe("number of trips and total price", function () {
+        it("if the user does not select return and take 2 trips to khayelitsha it should return R80", function () {
+            let busTravel = BusTicket();
+            busTravel.singleTrip(40, "khayelitsha");
+            busTravel.singleTrip(40, "khayelitsha");
+
+            assert.equal(80, busTravel.totalPriceOfSingle());
+        });
+
+        it("if the user takes 2 trips to mitchells plain and 1 trip to dunoon it should return R85", function () {
+            let busTravel = BusTicket();
+            busTravel.singleTrip(30, "mitchellsPlain");
+            busTravel.singleTrip(30, "mitchellsPlain");
+            busTravel.singleTrip(25, "dunoon");
+
+            assert.equal(85, busTravel.totalPriceOfSingle());
+        });
+        it("should return how many single trips a user had taken", function () {
+            let busTravel = BusTicket();
+            busTravel.singleTrip(30, "mitchellsPlain");
+            busTravel.singleTrip(30, "mitchellsPlain");
+            busTravel.singleTrip(25, "dunoon");
+            busTravel.singleTrip(40, "khayelitsha");
+            busTravel.singleTrip(40, "khayelitsha");
+
+
+            assert.equal(5, busTravel.amountOfSingle());
+        });
+        it("if a even number of single trips is made it should return how many return trips a user had taken", function () {
+            let busTravel = BusTicket();
+
+            busTravel.singleTrip(40, "khayelitsha");
+            busTravel.singleTrip(40, "khayelitsha");
+
+            busTravel.returnTrip();
+
+            assert.equal(1, busTravel.amountOfReturn());
+        });
+        it("if a odd number of single trips is made it should return how many return trips a user had taken", function () {
+            let busTravel = BusTicket();
+
+
+            busTravel.singleTrip(40, "khayelitsha");
+            busTravel.singleTrip(40, "khayelitsha");
+            busTravel.singleTrip(40, "khayelitsha");
+
+
+            busTravel.returnTrip();
+
+            assert.equal(2, busTravel.amountOfReturn());
+        });
+
+        it("should return how many price if return option is chosen", function () {
+            let busTravel = BusTicket();
+
+            busTravel.singleTrip(40, "khayelitsha");
+
+            busTravel.returnTrip();
+
+            assert.equal(80, busTravel.getReturnPrice());
+        });
+
+
+    });
+    describe("peak or no peak", function () {
+        it("if the user is travelling in peak time it should add 25 percent to the original price", function () {
+            let busTravel = BusTicket();
+            busTravel.singleTrip(40, "khayelitsha");
+            assert.equal(50, busTravel. addPeak());
+        });
+        it("if the user is travelling in peak time it should add 25 percent to the original price", function () {
+            let busTravel = BusTicket();
+
+            busTravel.singleTrip(25, "dunoon");
+            busTravel.singleTrip(40, "khayelitsha");
+            busTravel.singleTrip(40, "khayelitsha");
+
+            assert.equal(150, busTravel. addPeak());
+        });
+    });
+
+});
